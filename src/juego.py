952ae3a46f363ec69b4bafc205413838bdc08d41
tiolula <92263132+tiolula@users.noreply.github.com>
2022-09-21 15:20:15 -0300

    volviendo a la versión inicial de dojo (solo con el ejemplo)

--- src/juego.py
+++ src/juego.py
@@ -1,10 +1,2 @@
 def sumar(a, b):
     return a + b;
-
-def ganar(opcion_del_jugador_a, opcion_del_jugador_b):
-    if (opcion_del_jugador_a == "cafe"):
-        return "cafe"
-    if (opcion_del_jugador_a == "mate"):
-        return "mate"
-    if (opcion_del_jugador_a == "te"):
-        return "te"
--- tests/test_cachipum.py
+++ tests/test_cachipum.py
@@ -10,8 +10,7 @@ class TestDeCachipum(TestCase):
 # Arrange
 # Act
 # Assert
-# Lucero - Lula
-#Flor - Lucero
+
     #ARRANGE
     def test_ejemplo(self):
         a = 1
@@ -19,32 +18,5 @@ class TestDeCachipum(TestCase):
         esperado = 3
         # ACT
         resultado = juego.sumar(a, b)
-        #ASSERT
-        self.assertEqual(esperado, resultado)
-
-    def test_cafe_mata_mate(self):
-        opcion_del_jugador_a = "cafe"
-        opcion_del_jugador_b = "mate"
-        esperado = "cafe"
-        # ACT
-        resultado = juego.ganar(opcion_del_jugador_a, opcion_del_jugador_b)
-        #ASSERT
-        self.assertEqual(esperado, resultado)
-
-    def test_mate_mata_te(self):
-        opcion_del_jugador_a = "mate"
-        opcion_del_jugador_b = "te"
-        esperado = "mate"
-        # ACT
-        resultado = juego.ganar(opcion_del_jugador_a, opcion_del_jugador_b)
-        #ASSERT
-        self.assertEqual(esperado, resultado)
-
-    def test_te_mata_cafe(self):
-        opcion_del_jugador_a = "te"
-        opcion_del_jugador_b = "cafe"
-        esperado = "te"
-        # ACT
-        resultado = juego.ganar(opcion_del_jugador_a, opcion_del_jugador_b)
         #ASSERT
         self.assertEqual(esperado, resultado)
